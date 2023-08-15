9b4b7c2fc7aa1eeef97560a7cd1f754c98ba39dc
ikhela-04KK <kankoffi36@gmail.com>
2023-08-15 17:09:25 +0000

    Serie D'exerice sur EJS

---
+++ 4.1 EJS Tags/solution.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+// corrigé de index.js
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  const data = {
+    title: "EJS Tags",
+    seconds: new Date().getSeconds(),
+    items: ["apple", "banana", "cherry"],
+    htmlContent: "<em>This is some em text</em>",
+  };
+  res.render("solution.ejs", data);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
