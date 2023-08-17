da54766dfa0f609ee41259730ab5731a9592be77
ikhela-04KK <kankoffi36@gmail.com>
2023-08-17 08:06:41 +0000

    Combine ejs, bootstrap

--- extract.ps1
+++ extract.ps1
@@ -1,8 +1,9 @@
 # extraction de dossiers zip
 <#
 - Apprendre regex && syntaxe powershell
+- ^4\.[1-4] pour les documents commençant par 4.1 ... 4.4
 #>
 # permet d'extraire tous les dossiers dont le nom commence par un caractère regex spécifique 
-Get-ChildItem -Path 'C:\Users\Administrateur\Downloads' *.zip | Where-Object{$_.Name -match "^4\.[1-4]"} | ForEach-Object {
+Get-ChildItem -Path 'C:\Users\Administrateur\Downloads' *.zip | Where-Object{$_.Name -match "Drawkit"} | ForEach-Object {
     Expand-Archive -path $_.FullName -DestinationPath "C:\Users\Administrateur\Documents\nodeJS-modules-test\"
 }
