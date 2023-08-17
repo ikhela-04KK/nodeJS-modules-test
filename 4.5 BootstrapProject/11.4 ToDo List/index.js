1ab42ea5d030271b00cdf04a0b561d24a3e48272
ikhela-04KK <kankoffi36@gmail.com>
2023-08-17 23:24:35 +0000

    frustrating combinaison

---
+++ 4.5 BootstrapProject/11.4 ToDo List/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+// import {TreeWalker} from "tree-file";
+
+
+const app = express();
+const port = 3000;
+
+// TreeWalker("C:/Users/Administrateur/Documents/nodeJS-modules-test/4.5 BootstrapProject");
+
+app.use(express.static("public"));
+
+
+app.get("/", (req,res) =>{
+    res.render("index.ejs");
+})
+
+app.post("/features.ejs", (req, res) =>{
+    res.render("features.ejs")
+});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
+
+
+
