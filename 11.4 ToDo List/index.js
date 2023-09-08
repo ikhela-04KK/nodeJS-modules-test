84105d4120313005ff27e02018255afbcd7a2067
ikhela-04KK <kankoffi36@gmail.com>
2023-09-08 13:53:42 +0000

    add a new-commit

--- 11.4 ToDo List/index.js
+++ 11.4 ToDo List/index.js
@@ -11,7 +11,7 @@ const storage = multer.diskStorage({
     filename: function(req,file,cb){
         cb(null, Date.now() + path.extname(file.originalname));
     }
-+})
+})
 
 const upload = multer({storage:storage});
 const port = 3000;
