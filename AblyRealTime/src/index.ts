6a52a04e1a1b1942906c77c936f0bb5416955502
Koffi Kan Marc-Ephrem <102194271+ikhela-04KK@users.noreply.github.com>
2023-09-30 10:52:55 +0000

    Ably_real_time_test

---
+++ AblyRealTime/src/index.ts
@@ -0,0 +1,39 @@
+const Ably = require("ably");
+// Create the client
+const ably = new Ably.Realtime.Promise({
+  authUrl: "https://ably.com/ably-auth/token/docs",
+});
+
+// wrapper for async functions
+const ablyRealtimePromiseExample = async () => {
+  // Connect to Ably
+  await ably.connection.once("connected");
+  console.log("Connected to Ably!");
+
+  // get the channel to subscribe to
+  const channel = ably.channels.get("quickstart");
+
+  /* 
+    Subscribe to a channel. 
+    The promise resolves when the channel is attached 
+    (and resolves synchronously if the channel is already attached).
+  */
+  await channel.subscribe("greeting", (message) => {
+    console.log("Message is ==> " + message.data);
+  });
+
+  // Publish a message or two
+  await channel.publish("greeting", "hello!");
+  await channel.publish("greeting", "hello!");
+  await channel.publish("greeting", "hello!");
+
+  // wait 2s to receive all messages and then shut down
+  setTimeout(() => {
+    console.log("Closing connection...");
+    ably.close();
+    console.log("Closed the connection to Ably.");
+  }, 2000);
+};
+
+// call wrapper
+ablyRealtimePromiseExample();
